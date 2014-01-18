21f732e5ebe1a6ebb7e1e89a2636a7599253b425
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-01-18 14:41:15 +0100

    improved test speed

--- test/abdero-fetcher_test.js
+++ test/abdero-fetcher_test.js
@@ -5,21 +5,19 @@
  * Copyright (c) 2014 Andrea Parodi
  * Licensed under the MIT license.
  */
- 
+
 'use strict';
 
 
 var fs = require("fs"),
     abdero_fetcher = require("../lib/abdero-fetcher"),
     Fetcher = abdero_fetcher.Fetcher,
-    
-    expect = require("expect.js")
-   ;
+
+    expect = require("expect.js");
 
 
 
-
-describe("imapFetcher", function() {
+describe("abdero_fetcher", function() {
     describe("module", function() {
         it("is defined", function() {
             expect(abdero_fetcher).to.be.a("object");
@@ -28,6 +26,22 @@ describe("imapFetcher", function() {
     });
 
     describe("Fetcher", function() {
+        var transport;
+
+        before(function() {
+            transport = new Fetcher({
+                user: process.env.MY_MAIL_ADDRESS,
+                password: process.env.MY_MAIL_PASSWORD,
+                host: "imap.gmail.com",
+                port: 993,
+                tls: true,
+                tlsOptions: {
+                    rejectUnauthorized: false
+                }
+            });
+
+        });
+
         it("throw on non object option argument", function() {
             expect(function() {
                 return new Fetcher(null, "", "");
@@ -60,67 +74,49 @@ describe("imapFetcher", function() {
         });
 
 
-        var transport = new Fetcher({
-            user: process.env.MY_MAIL_ADDRESS,
-            password: process.env.MY_MAIL_PASSWORD,
-            host: "imap.gmail.com",
-            port: 993,
-            tls: true,
-            tlsOptions: {
-                rejectUnauthorized: false
-            }
-        });
 
         it("should be instantiable with config", function() {
             expect(transport).not.to.be.null;
         });
 
-        describe("status", function() {
-
-
-            it("should be disconnected at start", function() {
-                expect(transport.status).to.be.equal(abdero_fetcher.Status.disconnected);
-
-            });
-
-            it("go to connected upon connection", function(done) {
-                this.timeout(5000);
-                transport.connect(function() {
-                    expect(transport.status).to.be.equal(abdero_fetcher.Status.connected);
-                    transport.disconnect(function() {
-                        done();
-                    });
-
-                });
-
-
-            });
-
-            it("go to disconnected upon disconnection", function(done) {
-                this.timeout(25000);
-                transport.connect(function() {
-
-                    transport.disconnect(function() {
-                        expect(transport.status).to.be.equal(
-                            abdero_fetcher.Status.disconnected
-                        );
-                        done();
-                    });
-                });
-
-
-            });
+        it("should be disconnected at start", function() {
+            expect(transport.status).to.be.equal(abdero_fetcher.Status.disconnected);
 
         });
 
-        describe("list", function() {
-            var msgs;
-
+        describe("when connected", function() {
             before(function(done) {
-                this.timeout(25000);
-
+                this.timeout(5000);
 
                 transport.connect(function() {
+                    done();
+                });
+            });
+
+            after(function(done) {
+                this.timeout(5000);
+
+                transport.disconnect(function() {
+
+                    done();
+                });
+            });
+
+            it("status change to connected", function() {
+
+                expect(transport.status).to.be.equal(abdero_fetcher.Status.connected);
+
+
+            });
+
+            describe("list", function() {
+                var msgs;
+
+                before(function(done) {
+                    this.timeout(25000);
+
+
+
                     transport.list("INBOX", "1:*")
                         .then(function(msgsStream) {
 
@@ -131,9 +127,8 @@ describe("imapFetcher", function() {
                             });
                             msgsStream.once("end", function() {
 
-                                transport.disconnect(function() {
-                                    done();
-                                });
+
+                                done();
 
                             });
 
@@ -142,151 +137,146 @@ describe("imapFetcher", function() {
                     .then(null, function(err) {
                         console.log(err.stack);
                     });
+
+
                 });
 
+                it("return all messages in array", function() {
+
+                    expect(msgs.length).to.be.equal(5);
+
+
+                });
+
+                it("messages contains subject", function() {
+
+                    expect(msgs[0].subject).to.be.equal("this is a test");
+
+
+                });
+
+                it("messages contains uid", function() {
+
+                    expect(msgs[0].uid).to.be.a("number");
+                    expect(msgs[0].uid).to.be.equal(5);
+                    //                console.log(msgs[0].uid);
+
+
+                });
+
+                it("messages contains date", function() {
+
+                    expect(msgs[0].date).to.be.equal(1381592397000);
+
+
+                });
+
+
+
+                it("messages contains people", function() {
+
+                    expect(msgs[1].people.length).to.be.equal(2);
+
+
+                });
+
+
+                it("people has address", function() {
+
+                    expect(msgs[1].people[0].address).to.be.equal(
+                        "mail-noreply@google.com"
+                    );
+
+
+                });
+
+
+
+                it("people has name", function() {
+
+                    expect(msgs[1].people[0].name).to.be.equal("Team di Gmail");
+
+
+                });
             });
 
-            it("return all messages in array", function() {
+            describe("listBoxes", function() {
+                var boxesList;
 
-                expect(msgs.length).to.be.equal(5);
+                before(function(done) {
+                    this.timeout(25000);
 
-
-            });
-
-            it("messages contains subject", function() {
-
-                expect(msgs[0].subject).to.be.equal("this is a test");
-
-
-            });
-
-            it("messages contains uid", function() {
-
-                expect(msgs[0].uid).to.be.a("number");
-                expect(msgs[0].uid).to.be.equal(5);
-                //                console.log(msgs[0].uid);
-
-
-            });
-
-            it("messages contains date", function() {
-
-                expect(msgs[0].date).to.be.equal(1381592397000);
-
-
-            });
-
-
-
-            it("messages contains people", function() {
-
-                expect(msgs[1].people.length).to.be.equal(2);
-
-
-            });
-
-
-            it("people has address", function() {
-
-                expect(msgs[1].people[0].address).to.be.equal(
-                    "mail-noreply@google.com"
-                );
-
-
-            });
-
-
-
-            it("people has name", function() {
-
-                expect(msgs[1].people[0].name).to.be.equal("Team di Gmail");
-
-
-            });
-        });
-
-        describe("listBoxes", function() {
-            var boxesList;
-
-            before(function(done) {
-                this.timeout(25000);
-                transport.connect(function() {
                     transport.listBoxes()
                         .then(function(boxes) {
                             boxesList = boxes;
-                            //console.log(JSON.stringify(boxes));
-                            transport.disconnect(function() {
-                                done();
-                            });
+                            done();
                         })
 
                     .then(null, function(err) {
                         console.log(err.stack);
                     });
+
                 });
+
+
+                it("boxes has text property", function() {
+
+                    expect(boxesList[0].text).to.be.equal("INBOX");
+                });
+
+                it("boxes has id property", function() {
+
+                    expect(boxesList[0].id).to.be.equal("INBOX");
+                    expect(boxesList[0].children[0].id).to.be.equal("INBOX/test");
+                });
+
+                it("boxes has children", function() {
+
+                    expect(boxesList[0].children).to.be.an("array");
+                });
+
+                it("return all boxes", function() {
+
+                    expect(boxesList.length).to.be.equal(3);
+                });
+
+                it("return boxes array", function() {
+                    this.timeout(25000);
+                    expect(boxesList).to.be.an("array");
+                });
+
             });
 
-
-            it("boxes has text property", function() {
-
-                expect(boxesList[0].text).to.be.equal("INBOX");
-            });
-
-            it("boxes has id property", function() {
-
-                expect(boxesList[0].id).to.be.equal("INBOX");
-                expect(boxesList[0].children[0].id).to.be.equal("INBOX/test");
-            });
-
-            it("boxes has children", function() {
-
-                expect(boxesList[0].children).to.be.an("array");
-            });
-
-            it("return all boxes", function() {
-
-                expect(boxesList.length).to.be.equal(3);
-            });
-
-            it("return boxes array", function() {
-                this.timeout(25000);
-                expect(boxesList).to.be.an("array");
-            });
-
-        });
-
-        describe("fetch", function() {
+            describe("fetch", function() {
 
 
-            it("return message fetched promise", function(done) {
-                this.timeout(25000);
+                it("return message fetched promise", function(done) {
+                    this.timeout(25000);
 
 
-                transport.connect(function() {
+
                     transport.fetch("INBOX", 5)
                         .then(function(msg) {
                             //console.log(msg)
                             expect(msg.stream).to.not.equal(null);
                             expect(msg.info).to.not.equal(null);
-                            transport.disconnect(function() {
-                                done();
-                            });
+                            done();
 
                         })
 
                     .then(null, function(err) {
                         console.log(err.stack);
                     });
+
+
+
                 });
 
-
-            });
-
-            it("message stream contains full message body", function(done) {
-                this.timeout(25000);
+                it("message stream contains full message body", function(done) {
+                    this.timeout(25000);
 
 
-                transport.connect(function() {
+
                     transport.fetch("INBOX/test", 2)
                         .then(function(msg) {
 
@@ -305,40 +295,7 @@ describe("imapFetcher", function() {
                                 expect(actual).to.be.equal(
                                     expected.replace(/[\r\n]/g, "")
                                 );
-                                transport.disconnect(function() {
-                                    done();
-                                });
-                            });
 
-                        })
-
-                    .then(null, function(err) {
-                        console.log(err.stack);
-                    });
-                });
-
-
-            });
-        });
-
-        describe("download", function() {
-            var html,
-                text;
-
-            before(function(done) {
-                this.timeout(25000);
-
-
-                transport.connect(function() {
-                    transport.download("INBOX", 6)
-                        .then(function(msg) {
-                            var path = "./test/files/expected-body.html";
-                            var expected = fs.readFileSync(path, "utf8");
-
-                            html = msg.html.replace(/[\r\n ]/g, "");
-                            text = msg.text.replace(/[\r\n ]/g, "");
-
-                            transport.disconnect(function() {
                                 done();
                             });
 
@@ -347,38 +304,81 @@ describe("imapFetcher", function() {
                     .then(null, function(err) {
                         console.log(err.stack);
                     });
+
+
+
+                });
+            });
+
+            describe("download", function() {
+                var html,
+                    text;
+
+                before(function(done) {
+                    this.timeout(25000);
+
+
+                    transport.download("INBOX", 6)
+                        .then(function(msg) {
+                            var path = "./test/files/expected-body.html";
+                            var expected = fs.readFileSync(path, "utf8");
+
+                            html = msg.html.replace(/[\r\n ]/g, "");
+                            text = msg.text.replace(/[\r\n ]/g, "");
+
+                            done();
+
+                        })
+
+                    .then(null, function(err) {
+                        console.log(err.stack);
+                    });
+
+
+                });
+
+                it("return message body as html", function() {
+                    this.timeout(25000);
+                    var path = "./test/files/expected-body.html";
+                    var expected = fs.readFileSync(path, "utf8");
+                    expected = expected.replace(/[\r\n ]/g, "");
+
+
+
+                    expect(html).to.be.equal(expected);
+
+                });
+
+
+
+                it("return message body as text", function() {
+                    this.timeout(25000);
+                    var path = "./test/files/expected-body.txt";
+                    var expected = fs.readFileSync(path, "utf8");
+                    expected = expected.replace(/[\r\n ]/g, "");
+
+
+
+                    expect(text).to.be.equal(expected);
+
                 });
 
 
             });
 
-            it("return message body as html", function() {
-                this.timeout(25000);
-                var path = "./test/files/expected-body.html";
-                var expected = fs.readFileSync(path, "utf8");
-                expected = expected.replace(/[\r\n ]/g, "");
-
-
-
-                expect(html).to.be.equal(expected);
-
-            });
-
-
-
-            it("return message body as text", function() {
-                this.timeout(25000);
-                var path = "./test/files/expected-body.txt";
-                var expected = fs.readFileSync(path, "utf8");
-                expected = expected.replace(/[\r\n ]/g, "");
-
-
-
-                expect(text).to.be.equal(expected);
-
-            });
-
-
         });
+
+        describe("after disconnection", function() {
+            it("status change to disconnected", function() {
+
+                expect(transport.status).to.be.equal(
+                    abdero_fetcher.Status.disconnected
+                );
+
+
+            });
+        });
+
+
     });
 });
